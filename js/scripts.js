3dc732fb64794c9051fc516e91ab653019211502
pooleside <lpoole26@gmail.com>
2015-09-26 15:41:21 -0700

    "spec is passing"

--- js/scripts.js
+++ js/scripts.js
@@ -11,3 +11,16 @@ var pizza = function (size, price){
   returnString = "You ordered a " + newPizza.size + " pizza. Cost is " + newPizza.price;
   return returnString;
 }
+
+function Topping (name, price){
+  this.name = name;
+  this.price = price;
+
+}
+
+var topping = function (name, price){
+  var returnString = "";
+  var newTopping = new Topping(name, price);
+  returnString = "You added " + newTopping.name + " Cost is " + newTopping.price;
+  return returnString;
+}
