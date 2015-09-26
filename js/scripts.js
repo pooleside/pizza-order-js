7888134457d60f2ca97fc45334f581a4f7ac1a42
pooleside <lpoole26@gmail.com>
2015-09-26 16:36:38 -0700

    "specs passing"

--- js/scripts.js
+++ js/scripts.js
@@ -24,3 +24,15 @@ var topping = function (name, price){
   returnString = "You added " + newTopping.name + " Cost is " + newTopping.price;
   return returnString;
 }
+var tpizza = function addTopping (pizza, topping){ //objects
+  pizza.toppings.push(topping); //adds a topping into toppings arrary.  This brings pizza and topping together
+  return pizza;
+}
+var addTopping = function (size, price, toppingName, toppingPrice) {
+  var tempPizza = new Pizza (size, price);  //instance of pizza class
+  var tempTopping = new Topping (toppingName, toppingPrice); //parameter names
+  tempPizza.toppings.push(tempTopping);
+  // tempPizza = addTopping (tempPizza, tempTopping); //passing
+  var returnString = tempPizza.size + "," + tempPizza.price + "," + tempPizza.toppings[0].name + "," + tempPizza.toppings[0].price;
+    return returnString;
+}
